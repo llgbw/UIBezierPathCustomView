5a0ff8a6cbcb40dbcad4ee98d7d9feb09fc4d8a4
NYKJ <18820788880@163.com>
2018-04-23 14:33:28 +0800

    Initial Commit

---
+++ TestIrregularView/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TestIrregularView
+//
+//  Created by LuoKI on 2018/4/23.
+//  Copyright © 2018年 LuoLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ TestIrregularView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestIrregularView
+//
+//  Created by LuoKI on 2018/4/23.
+//  Copyright © 2018年 LuoLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
